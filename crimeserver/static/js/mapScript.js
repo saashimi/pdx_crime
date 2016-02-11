aaa69e4b1091678d9775c85cd6e38e9281e462e8
saashimi <saavedra.kevin@gmail.com>
2016-02-11 13:33:42 -0800

    Switch to bootstrap template.

--- crimeserver/static/js/mapScript.js
+++ crimeserver/static/js/mapScript.js
@@ -1,3 +1,64 @@
+      function applyMargins() {
+        var leftToggler = $(".mini-submenu-left");
+        if (leftToggler.is(":visible")) {
+          $("#map .ol-zoom")
+            .css("margin-left", 0)
+            .removeClass("zoom-top-opened-sidebar")
+            .addClass("zoom-top-collapsed");
+        } else {
+          $("#map .ol-zoom")
+            .css("margin-left", $(".sidebar-left").width())
+            .removeClass("zoom-top-opened-sidebar")
+            .removeClass("zoom-top-collapsed");
+        }
+      }
+      function isConstrained() {
+        return $(".sidebar").width() == $(window).width();
+      }
+      function applyInitialUIState() {
+        if (isConstrained()) {
+          $(".sidebar-left .sidebar-body").fadeOut('slide');
+          $('.mini-submenu-left').fadeIn();
+        }
+      }
+      $(function(){
+        $('.sidebar-left .slide-submenu').on('click',function() {
+          var thisEl = $(this);
+          thisEl.closest('.sidebar-body').fadeOut('slide',function(){
+            $('.mini-submenu-left').fadeIn();
+            applyMargins();
+          });
+        });
+        $('.mini-submenu-left').on('click',function() {
+          var thisEl = $(this);
+          $('.sidebar-left .sidebar-body').toggle('slide');
+          thisEl.hide();
+          applyMargins();
+        });
+        $(window).on("resize", applyMargins);
+        
+    var map = new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.Stamen({
+                    layer: 'toner'
+                })
+            }),
+            vectorLayer
+        ],
+        view: new ol.View({
+            projection: 'EPSG:3857',
+            center: ol.proj.fromLonLat([-122.6819, 45.5200]),
+            // KS we are reprojecting from geographic 4326 into web mercator;
+            zoom: 12,
+        })
+    });
+        applyInitialUIState();
+        applyMargins();
+      });
+
+
 var image = new ol.style.Circle({
     radius: 8,
     fillColor: 'red', //KS why isn't this working?
@@ -17,27 +78,6 @@ var styleFunction = function(feature) {
     return styles[feature.getGeometry().getType()];
 };
 
-/*var geojsonObject = { //KS: TODO:  turning this into a function
-    'type': 'FeatureCollection',
-    'crs': {
-      'type': 'name',
-      'properties': {
-        'name': 'EPSG:3857'
-      }
-    },
-    'features': [{
-      'type': 'Feature',
-      'geometry': {
-        'type': 'Point',
-        'coordinates': [0, 0]
-      }
-    }]
-};*/
-
-//var vectorSource = new ol.source.Vector({
-//    features:(new ol.format.GeoJSON()).readFeatures(geojsonObject)
-//});
-
 var vectorLayer = new ol.layer.Vector({
     title: 'added Layer',
     source: new ol.source.Vector({
@@ -49,22 +89,5 @@ var vectorLayer = new ol.layer.Vector({
 });
 
 
-var map = new ol.Map({
-    target: 'map',
-    layers: [
-        new ol.layer.Tile({
-            source: new ol.source.Stamen({
-                layer: 'toner-lite'
-            })
-        }),
-        vectorLayer
-    ],
-    view: new ol.View({
-        projection: 'EPSG:3857',
-        center: ol.proj.fromLonLat([-122.6819, 45.5200]),
-        // KS we are reprojecting from geographic 4326 into web mercator;
-        zoom: 12,
-    })
-});
 
 
