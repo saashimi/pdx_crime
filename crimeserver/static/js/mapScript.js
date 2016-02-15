b0e4b0f2abc3b3f0dfda2e61414d5c9fd590fb03
saashimi <saavedra.kevin@gmail.com>
2016-02-14 17:10:29 -0800

    Initial layers now selectable via checkbox.

--- crimeserver/static/js/mapScript.js
+++ crimeserver/static/js/mapScript.js
@@ -1,41 +1,72 @@
-var image = new ol.style.Circle({
+var homicideImage = new ol.style.Circle({
     radius: 8,
     fillColor: 'red', //KS why isn't this working?
     stroke: new ol.style.Stroke({
         color: 'red', 
         width:4
     })
-})
+});
+
+var DUIIImage = new ol.style.Circle({
+    radius: 2,
+    fillColor: 'green', //KS why isn't this working?
+    stroke: new ol.style.Stroke({
+        color: 'green', 
+        width:2
+    })
+});
 
 var styles = {
-    'Point': new ol.style.Style({
-        image: image
+    'DUII' : new ol.style.Style({
+      image: DUIIImage
+    }),
+    'Homicide': new ol.style.Style({
+      image: homicideImage
     }),
 };
 
-var styleFunction = function(feature) {
-    return styles[feature.getGeometry().getType()];
+var styleFunction = function(checkboxID) {
+    return styles[checkboxID];
 };
 
-var vectorLayer = new ol.layer.Vector({
-    title: 'added Layer',
+//var styleFunction = function(feature) {
+//    return styles[feature.getGeometry().getType()];
+//};
+
+function loadGeoJSON(checkboxID) {
+  //input is an array of params
+  serviceURL = 'http://localhost:8000/crimeserver/';
+  database = 'Crime2014'; // KS TODO: Differentiate by dropdown once we populate the DB.
+  trailer = '?format=json';
+  offenseID = '&offense=' + checkboxID;
+
+  var vectorLayer = new ol.layer.Vector({
+    title: checkboxID + ' Layer',
     source: new ol.source.Vector({
         projection: 'EPSG:3857',
-        url: 'http://localhost:8000/crimeserver/Crime2014?format=json&offense=Homicide',
-        format: new ol.format.GeoJSON()
+        url: serviceURL + database + trailer + offenseID,
+        format: new ol.format.GeoJSON(),
         }),
-    style: styleFunction
-});
+    style: styleFunction(checkboxID)
+  }); 
+  map.addLayer(vectorLayer);
+};
+
+function removeLayer(checkboxID) {
+  //something
+}
+
+// Emulating ESRI REST framework source
 
 var map = new ol.Map({
     target: 'map',
+    renderer: 'canvas', //KS force canvas. Might not be necessry.
     layers: [
-        new ol.layer.Tile({
-            source: new ol.source.Stamen({
-                layer: 'toner'
-            })
-        }),
-        vectorLayer
+      new ol.layer.Tile({
+          source: new ol.source.Stamen({
+              layer: 'toner'
+          })
+      }),
     ],
     view: new ol.View({
         projection: 'EPSG:3857',
@@ -44,6 +75,19 @@ var map = new ol.Map({
         zoom: 12,
     })
 });
+
+$(document).ready(function() {
+  $(":checkbox").click(function() {
+    var $this = $(this);
+    var checkID = $this.attr('id');
+    if ($this.is(':checked')) {
+      loadGeoJSON(checkID);  
+    } else {
+      console.log('unchecked fired!', checkID);
+      // Remove call remove layer function here
+    } 
+  });
+}); 
 
 
 // Copypasta from http://stackoverflow.com/questions/14925913/how-can-i-set-up-openlayers-to-use-backbone-js
