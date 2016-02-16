6a465df728ec8862ee65f53395f283c1195da794
saashimi <saavedra.kevin@gmail.com>
2016-02-16 09:57:35 -0800

    Add style definitions and lab notes.

---
+++ crimeserver/static/js/styleDefs.js
@@ -0,0 +1,202 @@
+var aggravatedAssaultImage = new ol.style.Circle({
+    radius: 3,
+    stroke: new ol.style.Stroke({
+        color: '#3399ff', 
+        width:1
+    })
+});
+
+var arsonImage = new ol.style.Circle({
+    radius: 3,
+    fill: new ol.style.Fill({
+        color: '#dc702a'
+    })
+});
+
+var simpleAssaultImage = new ol.style.Circle({
+    radius: 2,
+    fill: new ol.style.Fill({
+        color: '#ae0f83'
+    })
+}); 
+
+var burglaryImage = new ol.style.RegularShape({
+    fill: new ol.style.Fill({
+        color: '#ffc3a0'
+    }),
+    stroke: new ol.style.Stroke({
+        color: '#ffc3a0', 
+        width:2
+    }),
+    points: 4,
+    radius: 3,
+    radius2: 0,
+    angle: Math.PI / 4
+});
+
+var curfewImage = new ol.style.Circle({
+    radius: 3,
+    fill: new ol.style.Fill({
+        color: '#52361f'
+    })
+}); 
+
+var disorderlyConductImage = new ol.style.Circle({
+    radius: 3,
+    fill: new ol.style.Fill({
+        color: '#cbb946'
+    })
+}); 
+
+var drugsImage = new ol.style.Circle({
+    radius: 3,
+    fill: new ol.style.Fill({
+        color: '#f4dd2c'
+    })
+}); 
+
+var DUIIImage = new ol.style.Circle({
+    radius: 3,
+    fill: new ol.style.Fill({
+        color: '#85dead'
+    })
+});
+
+var embezzlementImage = new ol.style.Circle({
+    radius: 3,
+    fill: new ol.style.Fill({
+        color: '#797f8b'
+    })    
+});
+
+var forgeryImage = new ol.style.Circle({
+    radius: 3,
+    fill: new ol.style.Fill({
+        color: '#3b757a'
+    })    
+});    
+
+var fraudImage = new ol.style.RegularShape({
+    fill: new ol.style.Fill({
+        color: '#3399ff'
+    }),
+    points: 4,
+    radius: 4,
+    angle: Math.PI / 4
+});   
+
+var homicideImage = new ol.style.Circle({
+    radius: 8,
+    stroke: new ol.style.Stroke({
+        color: 'red', 
+        width:4
+    })
+});
+
+var kidnapImage = new ol.style.Circle({
+    radius: 3,
+    fill: new ol.style.Fill({
+        color: '#f4dd2c'
+    })
+});
+
+var larcenyImage = new ol.style.Circle({
+    radius: 3,
+    fill: new ol.style.Fill({
+        color: 'green'
+    })
+});
+
+var liquorLawsImage = new ol.style.Circle({
+    radius: 3,
+    fill: new ol.style.Fill({
+        color: 'green'
+    })
+});
+
+var motorVehicleTheftImage = new ol.style.Circle({
+    radius: 3,
+    fill: new ol.style.Fill({
+        color: 'green'
+    })
+});
+
+var offensesAgainstFamilyImage = new ol.style.Circle({
+    radius: 3,
+    fill: new ol.style.Fill({
+        color: 'green'
+    })
+});
+
+var prostitutionImage = new ol.style.Circle({
+    radius: 3,
+    fill: new ol.style.Fill({
+        color: 'green'
+    })
+});
+
+var rapeImage = new ol.style.Circle({
+    radius: 3,
+    fill: new ol.style.Fill({
+        color: 'green'
+    })
+});
+
+var robberyImage = new ol.style.Circle({
+    radius: 3,
+    stroke: new ol.style.Stroke({
+        color: '#e6776b', 
+        width:2
+    })
+});
+
+var runawayImage = new ol.style.Circle({
+    radius: 3,
+    fill: new ol.style.Fill({
+        color: 'green'
+    })
+});
+
+var sexOffensesImage = new ol.style.RegularShape({
+    fill: new ol.style.Fill({
+        color: '#92a8d1'
+    }),
+    points: 4,
+    radius: 4,
+    angle: Math.PI / 4
+})
+
+var stolenPropertyImage = new ol.style.Circle({
+    radius: 3,
+    fill: new ol.style.Fill({
+        color: 'green'
+    })
+});
+
+var tresspassImage = new ol.style.Circle({
+    radius: 3,
+    fill: new ol.style.Fill({
+        color: 'green'
+    })
+});
+
+var vandalismImage = new ol.style.Circle({
+    radius: 3,
+    fill: new ol.style.Fill({
+        color: 'green'
+    })
+});
+
+var weaponsImage = new ol.style.RegularShape({
+    fill: new ol.style.Fill({
+        color: 'red'
+    }),
+    stroke: new ol.style.Stroke({
+        color: 'red', 
+        width:2
+    }),
+    points: 4,
+    radius: 4,
+    radius2: 0,
+    angle: Math.PI / 4
+});
